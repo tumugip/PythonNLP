da300ca206ef1c145fe235b2b76d7142e6d124b4
mayu <m1716065tm@ug.jwu.ac.jp>
2020-02-21 17:25:56 +0900

    \n,\r,\u3000

--- mayu/wc.py
+++ mayu/wc.py
@@ -7,6 +7,10 @@ with open('/Users/mayu/Git/PythonNLP/data/kusamakura.txt') as f:
 with open('/Users/mayu/Git/PythonNLP/data/kusamakura.txt') as f:
     for line in f:
         l +=1
+
+text = re.sub("\n","",text)
+text = re.sub("\r","",text)
+text = re.sub("\u3000","",text)
 print("line:",l)
 word = len(text)
 print("words:",word)
