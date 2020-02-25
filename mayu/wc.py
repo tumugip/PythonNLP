4137c459c0901c89ac440e65cf59941eafdfbcad
mayu <m1716065tm@ug.jwu.ac.jp>
2020-02-25 16:26:11 +0900

    ()comp

--- mayu/wc.py
+++ mayu/wc.py
@@ -2,6 +2,7 @@ import re
 
 l = 0
 
+
 with open('/Users/mayu/Git/PythonNLP/data/kusamakura.txt') as f:
     text=f.read()
 with open('/Users/mayu/Git/PythonNLP/data/kusamakura.txt') as f:
@@ -11,6 +12,21 @@ with open('/Users/mayu/Git/PythonNLP/data/kusamakura.txt') as f:
 text = re.sub("\n","",text)
 text = re.sub("\r","",text)
 text = re.sub("\u3000","",text)
+while(1):
+    t = '※' in text
+    if t == False:
+        break
+    m = re.search(r'※',text)
+    n = re.search(r'\)',text)
+    p = m.start()
+    q = n.end()
+
+    a = text[:p]
+    b = text[q:]
+
+    text = a+b
+
 print("line:",l)
 word = len(text)
 print("words:",word)
+
