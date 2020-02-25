3a80e504c8cca1e01e5e486b2a3817ad7091eeea
mayu <m1716065tm@ug.jwu.ac.jp>
2020-02-25 17:57:34 +0900

    mecab

---
+++ mayu/mecab.py
@@ -0,0 +1,50 @@
+import MeCab
+import collections
+import pprint
+import re
+
+mecab = MeCab.Tagger('/usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+
+
+
+with open('/Users/mayu/Git/PythonNLP/data/kusamakura.txt') as f:
+    text=f.read()
+text = re.sub("\n","",text)
+text = re.sub("\r","",text)
+text = re.sub("\u3000","",text)
+while(1):
+    t = '※' in text
+    if t == False:
+        break
+    m = re.search(r'※',text)
+    n = re.search(r'\)',text)
+    p = m.start()
+    q = n.end()
+
+    a = text[:p]
+    b = text[q:]
+
+    text = a+b
+
+mecab.parse('')
+node = mecab.parseToNode(text)
+m = {}
+n=[]
+while node:
+    
+    word = node.surface
+    pos = node.feature.split(",")[0]
+    if pos =="名詞":
+        if(word in m):
+            m[word] += 1
+        else:
+            m[word] = 1
+    
+    node = node.next
+
+n=sorted(m.items(),key=lambda x:x[1],reverse=True)
+pprint.pprint(n)
+
+
+
+
